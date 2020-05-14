363247198fa40a8d10350eda0e039ed164109142
Rodrigo <fuentesosorio@fuentesosorio.com>
2020-05-13 21:30:24 -0400

    assets precompile test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-//= require rails-ujs
 //= require underscore/underscore
 //= require gmaps/google
-//= require activestorage
-//= require turbolinks
 //= require trix
 //= require_tree .
